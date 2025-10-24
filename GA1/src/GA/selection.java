5288be1eacb85d91553b15f68f57c60cd17b3fb7
FeDevv <ricobon03@gmail.com>
2025-10-24 10:42:20 +0200

    added file 'selection.java'

---
+++ GA1/src/GA/selection.java
@@ -0,0 +1,11 @@
+package GA;
+
+/**
+ * selezionare i migliori individui per dare il via
+ * ad una nuova generazione
+ * */
+public class selection {
+    //torneo binario
+    //se ho population size X voglio scegliere X coppie (2X individui)
+    //ogni coppia subisce crossover con probabilità P
+}
